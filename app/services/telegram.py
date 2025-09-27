19865359814af039f4a69d3361595d29fdf9115c
NikitaShuvalov <ktototam065@gmail.com>
2025-09-27 09:22:04 +0300

    telegram group ID.

--- app/services/telegram.py
+++ app/services/telegram.py
@@ -1,7 +1,7 @@
 import requests
 
 TELEGRAM_TOKEN = "7753186561:AAEr6OiezYlaOtN4Up2bg0HemY_eoOKMiPM"
-CHAT_ID = "752293440"
+CHAT_ID = "-1002576701610"
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
 
